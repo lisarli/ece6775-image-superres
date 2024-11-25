cad730d814f607e9c8a5545ee86eae1a6b714ff7
lrl74 <lrl74@cornell.edu>
2024-11-25 01:47:47 -0500

    Increase input image size to 20x20

--- implementation/ecelinux/typedefs.h
+++ implementation/ecelinux/typedefs.h
@@ -18,8 +18,8 @@ typedef ap_uint<32> bit32_t;
 typedef ap_fixed<32,8> pixel_type;
 
 const int SCALE_FACTOR = 2;
-const int ORIG_WIDTH = 4;
-const int ORIG_HEIGHT = 4;
+const int ORIG_WIDTH = 20;
+const int ORIG_HEIGHT = 20;
 
 #define FOR_PIXELS(r, c, chan, H, W) \
     for (int r = 0; r < H; ++r) \
